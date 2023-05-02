151ecf49252abc3228a8cf68e07d2a9c6a6dd44c
VishvamPatel31 <96796901+VishvamPatel31@users.noreply.github.com>
2023-05-02 11:14:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,54 @@
-# Project Yet to be completed
+import numpy as np
+import cv2
+
+# Initialize the camera capture object
+cap = cv2.VideoCapture(0)
+
+# Initialize the face and eye cascade classifiers
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# Loop indefinitely
+while True:
+    # Read the next frame from the camera
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale image
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Get the number of faces detected
+    num_faces = len(faces)
+
+    # Print the number of faces detected
+    print("Number of faces detected:", num_faces)
+
+    # Loop over each detected face
+    for (x, y, w, h) in faces:
+        # Draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+
+        # Extract the region of interest (ROI) corresponding to the face
+        roi_gray = gray[y:y+w, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        # Detect eyes in the face ROI
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+
+        # Loop over each detected eye
+        for (ex, ey, ew, eh) in eyes:
+            # Draw a rectangle around the eye
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+
+    # Display the annotated frame
+    cv2.imshow('frame', frame)
+
+    # Wait for a key press and check if it's the quit key
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the camera and close all windows
+cap.release()
+cv2.destroyAllWindows()
